44f83ef77930668a6800426a90675f33fbae0842
Aman Gupta <110199387+GuptaAman11@users.noreply.github.com>
2024-08-13 00:06:49 +0530

    Time: 28 ms (59.20%) | Memory: 25.2 MB (40.44%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans ;
-        map<string , vector<string>> hashmap ;
+        unordered_map<string , vector<string>> hashmap ;
         for(int i = 0 ; i < strs.size() ; i++){
             string temp = strs[i] ;
             sort(temp.begin() , temp.end()) ; 
